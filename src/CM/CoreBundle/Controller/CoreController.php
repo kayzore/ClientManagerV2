1b77595d6e1a90257e9cb4f0f57cc4a0a97b181e
Kayzore <jerome.lelievre2@gmail.com>
2017-01-19 19:51:27 +0100

    Ajout d'une verification sur chaque page pour savoir si l'utilisateur est connecté

--- src/CM/CoreBundle/Controller/CoreController.php
+++ src/CM/CoreBundle/Controller/CoreController.php
@@ -8,35 +8,54 @@ class CoreController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CMCoreBundle:membres:accueil.html.twig');
+        if ($this->get('security.context')->isGranted('ROLE_USER')) {
+            return $this->render('CMCoreBundle:membres:accueil.html.twig');
+        }
+        return $this->render('CMCoreBundle:visiteurs:accueil.html.twig');
     }
 
     public function addAction()
     {
-        return $this->render('CMCoreBundle:membres:ajouter.html.twig');
+        if ($this->get('security.context')->isGranted('ROLE_USER')) {
+            return $this->render('CMCoreBundle:membres:ajouter.html.twig');
+        }
+        $this->redirectToRoute('cm_core_homepage');
     }
 
     public function saveAddAction()
     {
+        if ($this->get('security.context')->isGranted('ROLE_USER')) {
+            return $this->render('CMCoreBundle:membres:ajouter.html.twig');
+        }
+        $this->redirectToRoute('cm_core_homepage');
         // Ajout en BDD d'un contact puis redirection
         $this->redirectToRoute('cm_core_homepage');
     }
 
     public function editAction($idContact)
     {
-        // Recuperation des informations du contact a modifier et affichage dans le formulaire
-        return $this->render('CMCoreBundle:membres:modifier.html.twig');
+        if ($this->get('security.context')->isGranted('ROLE_USER')) {
+            // Recuperation des informations du contact a modifier et affichage dans le formulaire
+            return $this->render('CMCoreBundle:membres:modifier.html.twig');
+        }
+        $this->redirectToRoute('cm_core_homepage');
     }
 
     public function saveEditAction($idContact)
     {
-        // Enregistrement des modifications sur un contact puis redirection
+        if ($this->get('security.context')->isGranted('ROLE_USER')) {
+            // Enregistrement des modifications sur un contact puis redirection
+            $this->redirectToRoute('cm_core_homepage');
+        }
         $this->redirectToRoute('cm_core_homepage');
     }
 
     public function deleteAction($idContact)
     {
-        // Suppression d'un contact puis redirection
+        if ($this->get('security.context')->isGranted('ROLE_USER')) {
+            // Suppression d'un contact puis redirection
+            $this->redirectToRoute('cm_core_homepage');
+        }
         $this->redirectToRoute('cm_core_homepage');
     }
 }
