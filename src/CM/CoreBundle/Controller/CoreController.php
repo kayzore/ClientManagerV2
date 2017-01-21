f1d8707f851ffe7e3c6068d1551c9262e144a5ce
Kayzore <jerome.lelievre2@gmail.com>
2017-01-21 19:46:09 +0100

    Ajout de la possibilité de modifier un contact

--- src/CM/CoreBundle/Controller/CoreController.php
+++ src/CM/CoreBundle/Controller/CoreController.php
@@ -45,21 +45,35 @@ class CoreController extends Controller
         return $this->redirectToRoute('cm_core_homepage');
     }
 
-    public function editAction($idContact)
+    public function editAction(Request $request, $idContact)
     {
         if ($this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
             // Recuperation des informations du contact a modifier et affichage dans le formulaire
-            return $this->render('CMCoreBundle:membres:modifier.html.twig');
+            $em = $this->getDoctrine()->getManager();
+            $contact = $em->getRepository('CMContactBundle:Contact')->findOneBy(array('id' => $idContact));
+            $contactNew = new Contact();
+            $formEditContact = $this->createForm(new ContactType(), $contactNew);
+            if ($formEditContact->handleRequest($request)->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $contact->setNom($formEditContact['nom']->getData());
+                $contact->setPrenom($formEditContact['prenom']->getData());
+                $contact->setEmail($formEditContact['email']->getData());
+                $birthdate = \DateTime::createFromFormat('d/m/Y', $formEditContact['birthdate']->getData());
+                $contact->setBirthdate($birthdate);
+                $contact->setAdresse($formEditContact['adresse']->getData());
+                $contact->setPostal($formEditContact['postal']->getData());
+                $contact->setVille($formEditContact['ville']->getData());
+                $contact->setTelephone($formEditContact['telephone']->getData());
+                $contact->setWebSite($formEditContact['webSite']->getData());
+                $em->persist($contact);
+                $em->flush();
+                return $this->redirect($this->generateUrl('cm_core_homepage'));
+            }
+            return $this->render('CMCoreBundle:membres:modifier.html.twig', array(
+                'formEditContact'   => $formEditContact->createView(),
+                'contact'           => $contact
+            ));
         }
-        return $this->redirectToRoute('cm_core_homepage');
-    }
-
-    public function saveEditAction($idContact)
-    {
-        if ($this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
-            // Enregistrement des modifications sur un contact
-        }
-        // puis redirection
         return $this->redirectToRoute('cm_core_homepage');
     }
 
