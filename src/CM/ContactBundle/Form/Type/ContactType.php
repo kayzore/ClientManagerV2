61c5acc0723722e0428841244807f28ff57a83c7
Kayzore <jerome.lelievre2@gmail.com>
2017-01-21 13:51:58 +0100

    Correction OptionsResolverInterface

--- src/CM/ContactBundle/Form/Type/ContactType.php
+++ src/CM/ContactBundle/Form/Type/ContactType.php
@@ -4,7 +4,7 @@ namespace CM\ContactBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContactType extends AbstractType
 {
@@ -28,9 +28,9 @@ class ContactType extends AbstractType
     }
     
     /**
-     * @param OptionsResolverInterface $resolver
+     * @param OptionsResolver $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'CM\ContactBundle\Entity\Contact'
